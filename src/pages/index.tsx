85df15d8f7b34ed04525870be19789d901c50a4b
fukuba-shuma <calculus.fukuba@gmail.com>
2022-11-06 20:58:09 +0900

    add head info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import Content from '~/components/organisms'
 
 const Home: NextPage = () => {
-  return <Content />
+  return (
+    <div>
+      <Head>
+        <title>Resas</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Content />
+    </div>
+  )
 }
 
 export default Home
